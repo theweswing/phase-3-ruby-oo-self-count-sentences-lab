5b1ed033e2f0eab52217d0c72642b07f8d300ed8
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-11-24 10:10:43 -0500

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self[-1] == '.' ? true : false
   end
 
   def question?
-
+    self[-1] == '?' ? true : false
   end
 
   def exclamation?
-
+    self[-1] == '!' ? true : false
   end
 
   def count_sentences
-
+    periods = self.split('.')
+    periods_points =
+      periods.collect { |given_string| given_string.split('!') }.flatten
+    periods_points_marks =
+      periods_points.collect { |given_string| given_string.split('?') }.flatten
+    flattened_and_split = periods_points_marks.flatten
+    flattened_and_split
+    flattened_and_split.length
   end
 end
